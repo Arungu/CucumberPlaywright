badd482e1bf54bd818a837cb5e3a78157b8d1a9c
guruarun1720@gmail.com <guruarun1720@gmail.com>
2024-01-12 14:46:01 +0530

    updated

---
+++ steps/LoginAdactinStep.js
@@ -0,0 +1,20 @@
+const {When, Then} = require('@cucumber/cucumber');
+const { LoginPage } = require('../pages/LoginPage');
+
+let lp =new LoginPage();
+
+When('User is on  login page',async()=>{
+    await lp.navigate();
+})
+When('User enter the username',async()=>{
+    await lp.enterUsername();
+})
+When('User enter the password',async()=>{
+    await lp.enterPassword();
+})
+When('User click on login button',async()=>{
+    await lp.clickOnLoginBtn();
+})
+Then('User verify the title',async()=>{
+    await lp.verifyTitle();
+})
